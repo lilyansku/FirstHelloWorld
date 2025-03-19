391044334c54694b620c739d087cd6810834a119
Angelika Ervasti <angelika.ervasti@hotmail.fi>
2025-03-19 09:20:11 +0200

    Final touches, did this project locally earlier but I commited it just now

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 function TodoList({ todos }) {
   return (
-    <ul>
-      {todos.map((task, index) => (
-        <li key={index}>{task}</li>
-      ))}
-    </ul>
+    <div>
+      <h2>ToDoList</h2>
+      <h3>Your tasks for today:</h3>
+      <ul>
+        {todos.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
